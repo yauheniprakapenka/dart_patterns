63b04adafda3422e8533f19c3298b0e11c04f5f5
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-01-04 15:23:51 +0300

    improve simple factory idiom

--- design_patterns/bin/pattern_simple_factory/factories/new_york_pizza_factory.dart
+++ design_patterns/bin/pattern_simple_factory/factories/new_york_pizza_factory.dart
@@ -3,6 +3,8 @@ import '../interfaces/i_pizza_factory.dart';
 import '../pizza/cheese_pizza.dart';
 import '../pizza/pepperoni_pizza.dart';
 
+// Этой пиццерии нужна фабрика для изготовления пиццы в нью-йоркском стиле: 
+// тонкая основа, изысканный соус и небольшое количество сыра.
 class NewYorkPizzaFactory implements IPizzaFactory {
   @override
   IPizza createPizza(String type) {
